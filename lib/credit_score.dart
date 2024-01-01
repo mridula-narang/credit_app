d419ad4f45b5a72100237a761f151c8f3db54d85
mridula-narang <narangmg@gmail.com>
2024-01-01 23:49:41 +0530

    commit5

--- lib/credit_score.dart
+++ lib/credit_score.dart
@@ -58,95 +58,97 @@ class _GaugeExampleState extends State<GaugeExample> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            SfRadialGauge(
-              animationDuration: 1500.00,
-              enableLoadingAnimation: true,
-              axes: <RadialAxis>[
-                RadialAxis(
-                  showLabels: false,
-                  showAxisLine: false,
-                  showTicks: false,
-                  minimum: 0,
-                  maximum: 99,
-                  ranges: <GaugeRange>[
-                    GaugeRange(
-                      startValue: 0,
-                      endValue: 33,
-                      color: Color(0xFFFE2A25),
-                      label: 'Low',
-                      sizeUnit: GaugeSizeUnit.factor,
-                      labelStyle: GaugeTextStyle(fontFamily: 'Helvetica', fontSize: 20),
-                      startWidth: 0.50,
-                      endWidth: 0.50,
-                    ),
-                    GaugeRange(
-                      startValue: 33,
-                      endValue: 66,
-                      color: Color(0xFFFFBA00),
-                      label: 'Average',
-                      labelStyle: GaugeTextStyle(fontSize: 20),
-                      startWidth: 0.50,
-                      endWidth: 0.50,
-                      sizeUnit: GaugeSizeUnit.factor,
-                    ),
-                    GaugeRange(
-                      startValue: 66,
-                      endValue: 99,
-                      color: Color(0xFF00AB47),
-                      label: 'Best',
-                      labelStyle: GaugeTextStyle(fontSize: 20),
-                      sizeUnit: GaugeSizeUnit.factor,
-                      startWidth: 0.50,
-                      endWidth: 0.50,
-                    ),
-                  ],
-                  pointers: <GaugePointer>[
-                    NeedlePointer(
-                      value: needleValue,
-                      enableAnimation: true,
-                      animationType: AnimationType.ease,
-                      animationDuration: 1500,
-                      needleLength: 0.60,
-                    ),
-                  ],
-                ),
-              ],
-            ),
-            SizedBox(height: 20),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: TextField(
-                controller: panController,
-                maxLength: 10,
-                decoration: InputDecoration(
-                  labelText: 'Enter PAN Number',
-                  border: OutlineInputBorder(borderRadius: BorderRadius.circular(10.0)),
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SfRadialGauge(
+                animationDuration: 1500.00,
+                enableLoadingAnimation: true,
+                axes: <RadialAxis>[
+                  RadialAxis(
+                    showLabels: false,
+                    showAxisLine: false,
+                    showTicks: false,
+                    minimum: 0,
+                    maximum: 99,
+                    ranges: <GaugeRange>[
+                      GaugeRange(
+                        startValue: 0,
+                        endValue: 33,
+                        color: Color(0xFFFE2A25),
+                        label: 'Low',
+                        sizeUnit: GaugeSizeUnit.factor,
+                        labelStyle: GaugeTextStyle(fontFamily: 'Helvetica', fontSize: 20),
+                        startWidth: 0.50,
+                        endWidth: 0.50,
+                      ),
+                      GaugeRange(
+                        startValue: 33,
+                        endValue: 66,
+                        color: Color(0xFFFFBA00),
+                        label: 'Average',
+                        labelStyle: GaugeTextStyle(fontSize: 20),
+                        startWidth: 0.50,
+                        endWidth: 0.50,
+                        sizeUnit: GaugeSizeUnit.factor,
+                      ),
+                      GaugeRange(
+                        startValue: 66,
+                        endValue: 99,
+                        color: Color(0xFF00AB47),
+                        label: 'Best',
+                        labelStyle: GaugeTextStyle(fontSize: 20),
+                        sizeUnit: GaugeSizeUnit.factor,
+                        startWidth: 0.50,
+                        endWidth: 0.50,
+                      ),
+                    ],
+                    pointers: <GaugePointer>[
+                      NeedlePointer(
+                        value: needleValue,
+                        enableAnimation: true,
+                        animationType: AnimationType.ease,
+                        animationDuration: 1500,
+                        needleLength: 0.60,
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+              SizedBox(height: 20),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextField(
+                  controller: panController,
+                  maxLength: 10,
+                  decoration: InputDecoration(
+                    labelText: 'Enter PAN Number',
+                    border: OutlineInputBorder(borderRadius: BorderRadius.circular(10.0)),
+                  ),
                 ),
               ),
-            ),
-            SizedBox(height: 10),
-            ElevatedButton(
-              onPressed: () {
-                validateAndRandomizePointer();
-                setState(() {});
-              },
-              child: Text('Get ECS'),
-            ),
-            SizedBox(height: 20),
-            Text(
-              'Available Payment Methods',
-              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-            ),
-            SizedBox(height: 10),
-            Text(
-              paymentOptions,
-              style: TextStyle(fontSize: 18),
-            ),
-            SizedBox(height: 10),
-          ],
+              SizedBox(height: 10),
+              ElevatedButton(
+                onPressed: () {
+                  validateAndRandomizePointer();
+                  setState(() {});
+                },
+                child: Text('Get ECS'),
+              ),
+              SizedBox(height: 20),
+              Text(
+                'Available Payment Methods',
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              ),
+              SizedBox(height: 10),
+              Text(
+                paymentOptions,
+                style: TextStyle(fontSize: 18),
+              ),
+              SizedBox(height: 10),
+            ],
+          ),
         ),
       ),
     );
